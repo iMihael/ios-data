37dfda94e279a16905a330a29dc70d44a2ea2800
ItCraft_dev8 <itcraft_dev8@ItCraft-dev8s-Mac-mini.local>
2015-01-22 16:16:07 +0200

    Initial Commit

---
+++ data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  data
+//
+//  Created by ItCraft_dev8 on 1/22/15.
+//  Copyright (c) 2015 ItCraft_dev8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
