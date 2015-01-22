2b44dc4ce8c894362e6641e5ad5e0c18cda819d2
ItCraft_dev8 <itcraft_dev8@ItCraft-dev8s-Mac-mini.local>
2015-01-22 19:48:50 +0200

    add core data save functionality

--- data/ViewController.swift
+++ data/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var addButton: UIButton!
+    @IBOutlet var deleteButton: UIButton!
+    @IBOutlet var table: UITableView!
+    
+    var data: [String] = ["Plums", "Apples", "Pears", "Bananas"]
+    var appDel:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+    var context:NSManagedObjectContext = NSManagedObjectContext()
+    
+    @IBAction func addAction() {
+        var newFruit = NSEntityDescription.insertNewObjectForEntityForName("Fruits", inManagedObjectContext: self.context) as NSManagedObject
+        newFruit.setValue(textField.text, forKey: "name")
+        newFruit.setValue(NSDate(), forKey: "created_at")
+        
+        context.save(nil)
+        println(newFruit)
 
+        data.append(String(textField.text))
+        table.reloadData()
+        textField.text = ""
+    }
+    
+    @IBAction func deleteAction() {
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = self.data[indexPath.row]
+        return cell
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.context = appDel.managedObjectContext!
+        
+        var request = NSFetchRequest(entityName: "Fruits")
+        request.returnsObjectsAsFaults = false
+        
+        var results:NSArray = context.executeFetchRequest(request, error: nil)!
+        if results.count > 0 {
+            data = []
+            for res in results {
+                println(res)
+                data.append(String(res.name))
+            }
+        }
+        println(data)
+        table.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
 
 }
 
